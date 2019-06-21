248e5e3bcac329a61218c2760157956fa50d14f1
Joshua Balentine <BroadSight@users.noreply.github.com>
2019-06-21 18:41:44 -0500

    start timer toggle

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace wrike_timer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string PUNCT_SPACE = " ";
+
+        private bool _timerRunning = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void dragHandle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+      private void timerToggle_Click(object sender, RoutedEventArgs e)
+      {
+         if (this._timerRunning)
+         {
+            this.timerAmount.Content = $"0{PUNCT_SPACE}00";
+            this.timerIcon.Style = (Style)this.Resources["pausePathStyle"];
+         }
+         else
+         {
+            this.timerAmount.Content = "0:00";
+            this.timerIcon.Style = (Style)this.Resources["playPathStyle"];
+         }
+         this._timerRunning = !this._timerRunning;
+      }
     }
 }
